6547c146d0c61f5c2399949815527dc8f6162da7
Lucas Sinclair <lucas.sinclair@me.com>
2016-03-04 13:45:04 +0100

    Updated git

--- plumbing/git.py
+++ plumbing/git.py
@@ -19,31 +19,35 @@ class GitRepo(DirectoryPath):
         # Check exists #
         if not os.path.exists(self.git_dir):
             raise Exception("No git repository at '%s'" % (self.git_dir))
+        # Default arguments #
+        self.default = ["--git-dir=" + self.git_dir, "--work-tree=" + self.path]
 
     @property
     def tag(self):
-        tag = sh.git("--git-dir=" + self.git_dir,
-                     "--work-tree=" + self.path,
-                     "describe", "--tags", "--dirty", "--always")
+        """For instance: u'1.0.3-69-gf0c796d-dirty'"""
+        tag = sh.git(self.default + ["describe", "--tags", "--dirty", "--always"])
         return tag.strip('\n')
 
     @property
     def hash(self):
-        sha1 = sh.git("--git-dir=" + self.git_dir,
-                      "--work-tree=" + self.path,
-                      "rev-parse", "HEAD")
+        """For instance: u'f0c796dae64a5a118d88e60523c011d535e8c476'"""
+        sha1 = sh.git(self.default + ["rev-parse", "HEAD"])
+        return sha1.strip('\n')
+
+    @property
+    def short_hash(self):
+        """For instance: u'f0c796dae64a5a118d88e60523c011d535e8c476'"""
+        sha1 = sh.git(self.default + ["rev-parse", "--short", "HEAD"])
         return sha1.strip('\n')
 
     @property
     def branch(self):
-        result = sh.git("--git-dir=" + self.git_dir,
-                        "--work-tree=" + self.path,
-                        'symbolic-ref', '--short', 'HEAD').strip('\n')
+        """For instance: u'master'"""
+        result = sh.git(self.default + ['symbolic-ref', '--short', 'HEAD'])
         return result.strip('\n')
 
     @property
     def remote_branch(self):
-        result = sh.git("--git-dir=" + self.git_dir,
-                        "--work-tree=" + self.path,
-                        'rev-parse', '--symbolic-full-name', '--abbrev-ref', '@{u}').strip('\n')
+        """For instance: u'origin/master'"""
+        result = sh.git(self.default + ['rev-parse', '--symbolic-full-name', '--abbrev-ref', '@{u}'])
         return result.strip('\n')
