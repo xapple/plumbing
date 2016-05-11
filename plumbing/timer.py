8fbb3616536a3a36da820c2d676cd216d5596d3b
Lucas Sinclair <lucas.sinclair@me.com>
2016-05-11 17:32:30 +0200

    Better timmer

--- plumbing/timer.py
+++ plumbing/timer.py
@@ -6,7 +6,21 @@ from plumbing.color import Color
 
 ################################################################################
 class Timer(object):
-    """Useful for timing the different steps in a pipeline"""
+    """Useful for timing the different steps in a pipeline.
+
+    Use it like this:
+
+        timer = Timer()
+        timer.print_start()
+        timer.print_elapsed()
+        timer.print_end()
+        timer.print_total_elapsed()
+
+    Or like this:
+
+        with Timer():
+            time.sleep(2)
+    """
 
     def __init__(self):
         self.start_time = datetime.now()
@@ -20,15 +34,15 @@ class Timer(object):
 
     def print_elapsed(self, reset=True):
         print self.prefix + "Elapsed time: %s" % (datetime.now() - self.last_mark) + self.suffix
-        self.last_mark = datetime.now()
+        if reset: self.last_mark = datetime.now()
 
     def print_total_elapsed(self, reset=True):
         print self.prefix + "Total elapsed time: %s" % (datetime.now() - self.start_time) + self.suffix
-        self.last_mark = datetime.now()
+        if reset: self.last_mark = datetime.now()
 
     @property
     def color(self):
-        """Should be use color or not ? If we are not in a shell like ipython then not"""
+        """Should we use color or not ? If we are not in a shell like ipython then not"""
         import __main__ as main
         if not hasattr(main, '__file__'): return True
         return False
@@ -37,3 +51,13 @@ class Timer(object):
     def prefix(self): return "" if not self.color else Color.grn
     @property
     def suffix(self): return "" if not self.color else Color.end
+
+    def __enter__(self):
+        """Set the start time"""
+        self.start_time = datetime.now()
+        self.print_start()
+        return self
+
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        self.print_end()
+        self.print_total_elapsed()
