b7997801a8fb3c234a0422c8a01ee6aae98b4a1d
Lucas Sinclair <lucas.sinclair@me.com>
2016-05-27 21:26:34 +0200

    "Back to development version."

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name             = 'plumbing',
-    version          = '2.0.6',
+    version          = '2.0.7',
     description      = 'Helps with plumbing-type programing in python.',
     license          = 'MIT',
     url              = 'http://github.com/xapple/plumbing/',
