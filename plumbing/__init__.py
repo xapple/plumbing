2d001337417a6d3e5f9eddd6b0bf04ae7861b8b2
Lucas Sinclair <lucas.sinclair@me.com>
2016-05-31 15:21:41 +0200

    "Back to development version."

--- plumbing/__init__.py
+++ plumbing/__init__.py
@@ -1,7 +1,7 @@
 b'This module needs Python 2.7.x'
 
 # Special variables #
-__version__ = '2.0.8'
+__version__ = '2.0.9'
 
 # Expose objects #
 from cmd import command
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name             = 'plumbing',
-    version          = '2.0.8',
+    version          = '2.0.9',
     description      = 'Helps with plumbing-type programing in python.',
     license          = 'MIT',
     url              = 'http://github.com/xapple/plumbing/',
