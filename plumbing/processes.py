5c9f049279e5cec717b1ab58aeda6f6b79c9face
Lucas Sinclair <lucas.sinclair@me.com>
2016-12-16 18:40:43 +0100

    Better prll_map

--- plumbing/processes.py
+++ plumbing/processes.py
@@ -3,7 +3,7 @@ Processes and parallelism
 =========================
 
 The ``processes`` module provides some convenience functions
-for using processes in python.
+for using parallel processes in python.
 
 Adapted from http://stackoverflow.com/a/16071616/287297
 
@@ -19,7 +19,8 @@ Comments:
 
 Pitfalls: all the stdouts are sent back to the parent stdout, intertwined.
 
-Alternatively, use this fork of multiprocessing: https://github.com/uqfoundation/multiprocess
+Alternatively, use this fork of multiprocessing:
+https://github.com/uqfoundation/multiprocess
 """
 
 # Modules #
@@ -27,34 +28,43 @@ import multiprocessing
 from tqdm import tqdm
 
 ################################################################################
-def target_func(f, q_in, q_out):
-    while True:
-        i, x = q_in.get()
-        if i is None: break
-        q_out.put((i, f(x)))
+def apply_function(func_to_apply, queue_in, queue_out):
+    while not queue_in.empty():
+        num, obj = queue_in.get()
+        queue_out.put((num, func_to_apply(obj)))
 
 ################################################################################
-def prll_map(func_to_apply, items, cpus=None, verbose=False):
-    # Number of cores #
+def prll_map(func_to_apply, items, cpus=None, verbose=True):
+    # Number of processes to use #
     if cpus is None: cpus = min(multiprocessing.cpu_count(), 32)
     # Create queues #
-    q_in = multiprocessing.Queue(1)
+    q_in  = multiprocessing.Queue()
     q_out = multiprocessing.Queue()
     # Process list #
-    new_proc = lambda t,a: multiprocessing.Process(target=t, args=a)
-    processes = [new_proc(target_func, (func_to_apply, q_in, q_out)) for x in range(cpus)]
+    new_proc  = lambda t,a: multiprocessing.Process(target=t, args=a)
+    processes = [new_proc(apply_function, (func_to_apply, q_in, q_out)) for x in range(cpus)]
+    # Put all the items (objects) in the queue #
+    sent = [q_in.put((i, x)) for i, x in enumerate(items)]
     # Start them all #
     for proc in processes:
         proc.daemon = True
         proc.start()
-    # Put and get #
-    sent = [q_in.put((i, x)) for i, x in enumerate(items)]
-    for x in range(cpus): q_in.put((None, None))
-    res  = [q_out.get() for x in range(len(sent))]
-    # Wait for them to finish #
+    # Display progress bar or not #
     if verbose:
-        for proc in tqdm(processes): proc.join()
+        results = [q_out.get() for x in tqdm(range(len(sent)))]
     else:
-        for proc in processes: proc.join()
+        results = [q_out.get() for x in range(len(sent))]
+    # Wait for them to finish #
+    for proc in processes: proc.join()
     # Return results #
-    return [x for i, x in sorted(res)]
+    return [x for i, x in sorted(results)]
+
+################################################################################
+def test():
+    def slow_square(x):
+        import time
+        time.sleep(2)
+        return x**2
+    objs    = range(20)
+    squares = prll_map(slow_square, objs, 4, verbose=True)
+    print "Result: %s" % squares
