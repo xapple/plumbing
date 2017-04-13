dad3a70d6fe3677f8324c1f177831d7eedd3fac5
Lucas Sinclair <lucas.sinclair@me.com>
2017-04-13 09:32:28 +0200

    Test file for parallel map

---
+++ tests/processes/test_command.py
@@ -0,0 +1,14 @@
+# Built-in module #
+
+# Internal modules #
+from plumbing.processes import prll_map
+
+def add(xy):
+    x,y = xy
+    return x+y
+
+inputs = [(5,5), (2,2), (20,30)]
+
+results = prll_map(add, inputs)
+
+print results
