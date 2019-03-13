76014b9da02d54f91216690ecf82a4b210f438cb
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-03-13 14:26:38 +0100

    Version 2.1.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'plumbing',
-    version          = '2.1.6',
+    version          = '2.1.7',
     description      = 'Helps with plumbing-type programing in python.',
     license          = 'MIT',
     url              = 'http://github.com/xapple/plumbing/',
