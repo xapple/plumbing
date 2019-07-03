ab0a745d3c6bc9e46ffb3e70bab159ac0d4aae60
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-07-03 14:30:56 +0200

    Added count_unique_index for dataframes

--- plumbing/dataframes.py
+++ plumbing/dataframes.py
@@ -20,3 +20,34 @@ def pandas_df_to_r_df(pandas_df):
 def pandas_df_to_named_r_df(pandas_df, r_name):
     eval("%s = convert_to_r_dataframe(pandas_df)" % r_name)
     eval("return %s" % r_name)
+
+################################################################################
+def count_unique_index(df, by):
+  """
+  This function enables you to quickly see how many unique combinations of
+  column values exist in a data frame. Here are two examples:
+
+      >>> import seaborn
+      >>> tips = seaborn.load_dataset("tips")
+      >>> print(count_unique_index(df, ['sex', 'smoker']))
+            sex smoker  count
+      0    Male    Yes     60
+      1    Male     No     97
+      2  Female    Yes     33
+      3  Female     No     54
+
+      >>> tips = seaborn.load_dataset("tips")
+      >>> print(count_unique_index(df, ['sex', 'smoker', 'day']))
+            sex smoker    time  count
+      0    Male    Yes   Lunch     13
+      1    Male    Yes  Dinner     47
+      2    Male     No   Lunch     20
+      3    Male     No  Dinner     77
+      4  Female    Yes   Lunch     10
+      5  Female    Yes  Dinner     23
+      6  Female     No   Lunch     25
+      7  Female     No  Dinner     29
+  """
+  return df.groupby(by).size().reset_index().rename(columns={0: 'count'})
+
+
