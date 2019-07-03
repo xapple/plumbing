256109b1399dba7055510ef6192deb1b80d4e7a1
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2019-07-03 15:31:54 +0200

    Added method to define data frames with strings

--- plumbing/dataframes.py
+++ plumbing/dataframes.py
@@ -5,6 +5,7 @@
 # Third party modules #
 import pandas, numpy
 #from pandas.rpy.common import convert_to_r_dataframe
+from six import StringIO
 
 ################################################################################
 def r_matrix_to_dataframe(matrix):
@@ -22,32 +23,50 @@ def pandas_df_to_named_r_df(pandas_df, r_name):
     eval("return %s" % r_name)
 
 ################################################################################
+def string_to_df(string):
+    """
+    Parse a string as a dataframe. Example:
+
+        >>> a = '''  i  | x | y | z
+        >>>         AR  | v | 5 | 1
+        >>>         For | w | 3 | 3
+        >>>         For | w | 4 | 4 '''
+        >>> df = string_to_df(a)
+        >>> print(df)
+             i  x  y  z
+        0   AR  v  5  1
+        1  For  w  3  3
+        2  For  w  4  4
+    """
+    return pandas.read_csv(StringIO(string.replace(' ','')), sep="|", header=0)
+
+################################################################################
 def count_unique_index(df, by):
-  """
-  This function enables you to quickly see how many unique combinations of
-  column values exist in a data frame. Here are two examples:
+    """
+    This function enables you to quickly see how many unique combinations of
+    column values exist in a data frame. Here are two examples:
 
-      >>> import seaborn
-      >>> tips = seaborn.load_dataset("tips")
-      >>> print(count_unique_index(df, ['sex', 'smoker']))
-            sex smoker  count
-      0    Male    Yes     60
-      1    Male     No     97
-      2  Female    Yes     33
-      3  Female     No     54
+        >>> import seaborn
+        >>> tips = seaborn.load_dataset("tips")
+        >>> print(count_unique_index(df, ['sex', 'smoker']))
+              sex smoker  count
+        0    Male    Yes     60
+        1    Male     No     97
+        2  Female    Yes     33
+        3  Female     No     54
 
-      >>> tips = seaborn.load_dataset("tips")
-      >>> print(count_unique_index(df, ['sex', 'smoker', 'day']))
-            sex smoker    time  count
-      0    Male    Yes   Lunch     13
-      1    Male    Yes  Dinner     47
-      2    Male     No   Lunch     20
-      3    Male     No  Dinner     77
-      4  Female    Yes   Lunch     10
-      5  Female    Yes  Dinner     23
-      6  Female     No   Lunch     25
-      7  Female     No  Dinner     29
-  """
-  return df.groupby(by).size().reset_index().rename(columns={0: 'count'})
+        >>> tips = seaborn.load_dataset("tips")
+        >>> print(count_unique_index(df, ['sex', 'smoker', 'day']))
+              sex smoker    time  count
+        0    Male    Yes   Lunch     13
+        1    Male    Yes  Dinner     47
+        2    Male     No   Lunch     20
+        3    Male     No  Dinner     77
+        4  Female    Yes   Lunch     10
+        5  Female    Yes  Dinner     23
+        6  Female     No   Lunch     25
+        7  Female     No  Dinner     29
+    """
+    return df.groupby(by).size().reset_index().rename(columns={0: 'count'})
 
 
