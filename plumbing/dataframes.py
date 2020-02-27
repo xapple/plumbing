5a225d09b56fb97702aa64b7c261966561326fee
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-02-27 19:17:36 +0800

    Add a multi_index_pivot function for dataframes

--- plumbing/dataframes.py
+++ plumbing/dataframes.py
@@ -4,7 +4,6 @@
 
 # Third party modules #
 import pandas, numpy
-#from pandas.rpy.common import convert_to_r_dataframe
 from six import StringIO
 
 ################################################################################
@@ -15,6 +14,7 @@ def r_matrix_to_dataframe(matrix):
 
 ################################################################################
 def pandas_df_to_r_df(pandas_df):
+    from pandas.rpy.common import convert_to_r_dataframe
     return convert_to_r_dataframe(pandas_df)
 
 ################################################################################
@@ -85,3 +85,35 @@ def count_unique_index(df, by):
     return df.groupby(by).size().reset_index().rename(columns={0: 'count'})
 
 
+###############################################################################
+def multi_index_pivot(df, columns=None, values=None):
+    """
+    Pivot a pandas data frame from long to wide format on multiple index variables.
+    Copied from https://github.com/pandas-dev/pandas/issues/23955
+
+    Note: you can perform the opposite operation, i.e.
+    unpivot a DataFrame from wide format to long format with df.melt().
+    In contrast to `pivot`, `melt` does acccept a multiple index specified
+    as the `id_vars` argument.
+
+    TODO: add warning when there is no index set.
+
+    Otherwise the error message is cryptic:
+    KeyError: "None of [Index([None], dtype='object')] are in the [columns]"
+
+    TODO: add example use for this function
+    """
+    names        = list(df.index.names)
+    df           = df.reset_index()
+    list_index   = df[names].values
+    tuples_index = [tuple(i) for i in list_index] # hashable
+    df           = df.assign(tuples_index=tuples_index)
+    df           = df.pivot(index="tuples_index", columns=columns, values=values)
+    tuples_index = df.index  # reduced
+    index        = pandas.MultiIndex.from_tuples(tuples_index, names=names)
+    df.index     = index
+    # Remove confusing index column name #
+    df.columns.name = None
+    df = df.reset_index()
+    # Return #
+    return df
