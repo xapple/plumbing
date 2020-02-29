d92dfb07e1e16a5bfef87919c44d7f28ceb5febd
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-02-29 17:51:25 +0800

    Update documentation for multi_index_pivot

--- plumbing/dataframes.py
+++ plumbing/dataframes.py
@@ -89,19 +89,25 @@ def count_unique_index(df, by):
 def multi_index_pivot(df, columns=None, values=None):
     """
     Pivot a pandas data frame from long to wide format on multiple index variables.
-    Copied from https://github.com/pandas-dev/pandas/issues/23955
 
-    Note: you can perform the opposite operation, i.e.
-    unpivot a DataFrame from wide format to long format with df.melt().
-    In contrast to `pivot`, `melt` does acccept a multiple index specified
+    Copied from: https://github.com/pandas-dev/pandas/issues/23955
+
+    Note: you can perform the opposite operation, i.e. unpivot a DataFrame from
+    wide format to long format with df.melt().
+
+    In contrast to `pivot`, `melt` does accept a multiple index specified
     as the `id_vars` argument.
-
-    TODO: add warning when there is no index set.
 
     Otherwise the error message is cryptic:
     KeyError: "None of [Index([None], dtype='object')] are in the [columns]"
 
-    TODO: add example use for this function
+    TODO: add warning when there is no index set.
+
+    Usage:
+
+        >>> df.multiindex_pivot(index   = ['idx_column1', 'idx_column2'],
+        >>>                     columns = ['col_column1', 'col_column2'],
+        >>>                     values  = 'bar')
     """
     names        = list(df.index.names)
     df           = df.reset_index()
