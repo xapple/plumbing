74d64995d4e132e6970e5d867249b4a23ddd9e0c
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-05-15 18:33:39 +0200

    Version 2.6.9

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'plumbing',
-    version          = '2.6.8',
+    version          = '2.6.9',
     description      = 'Helps with plumbing-type programing in python.',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
