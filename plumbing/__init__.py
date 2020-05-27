95fdb33177a6255406f93c7dfdd6a36e5866004b
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-05-27 13:23:52 +0200

    Back to development version.

--- plumbing/__init__.py
+++ plumbing/__init__.py
@@ -1,2 +1,2 @@
 # Special variables #
-__version__ = '2.7.7'
+__version__ = '2.7.8'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'plumbing',
-    version          = '2.7.7',
+    version          = '2.7.8',
     description      = 'Helps with plumbing-type programing in python.',
     long_description = open('README.md').read(),
     long_description_content_type = 'text/markdown',
