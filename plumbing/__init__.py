95a212bf0398ad9f08da3cfa103d7c72464b8642
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2020-07-20 14:01:07 +0200

    Version 2.8.5

--- plumbing/__init__.py
+++ plumbing/__init__.py
@@ -1,2 +1,2 @@
 # Special variables #
-__version__ = '2.8.4'
+__version__ = '2.8.5'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name             = 'plumbing',
-    version          = '2.8.4',
+    version          = '2.8.5',
     description      = 'Helps with plumbing-type programing in python.',
     license          = 'MIT',
     url              = 'http://github.com/xapple/plumbing/',
