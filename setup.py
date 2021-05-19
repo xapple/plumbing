644d7c86022fc9d47f43e567fe09333cc4fc38bc
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-19 23:01:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,22 @@
-from setuptools import setup, find_namespace_packages
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
+"""
+Written by Lucas Sinclair.
+MIT Licensed.
+Contact at www.sinclair.bio
+"""
+
+# Imports #
+from setuptools import setup, find_namespace_packages
+from os import path
+
+# Load the contents of the README file #
+this_dir = path.abspath(path.dirname(__file__))
+readme_path = path.join(this_dir, 'README.md')
+with open(readme_path, encoding='utf-8') as handle: readme = handle.read()
+
+# Call setup #
 setup(
     name             = 'plumbing',
     version          = '2.9.11',
@@ -8,10 +25,11 @@ setup(
     url              = 'http://github.com/xapple/plumbing/',
     author           = 'Lucas Sinclair',
     author_email     = 'lucas.sinclair@me.com',
-    packages         = find_namespace_packages(),
+    packages         = find_namespace_packages(exclude=['testing']),
     install_requires = ['autopaths>=1.4.2', 'six', 'pandas', 'numpy', 'matplotlib',
                         'retry', 'tzlocal', 'packaging', 'requests'],
-    long_description = open('README.md').read(),
+    python_requires  = ">=3.6",
+    long_description = readme,
     long_description_content_type = 'text/markdown',
     include_package_data = True,
 )
