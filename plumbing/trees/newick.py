47b6e911ab9240bd77ed3dfc89ced98609eb6460
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-19 23:17:33 +0200

    Removed lots of old functionality

--- plumbing/trees/newick.py
+++
@@ -1,6 +0,0 @@
-"""http://stackoverflow.com/a/23173968/287297"""
-
-################################################################################
-def parse_newick(path):
-    """Parse a newick-formated text file and return a "Tree" object."""
-    pass
