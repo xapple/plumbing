2dbe01972e9795872627a7422e36a21261c67884
Lucas Sinclair <649288+xapple@users.noreply.github.com>
2021-05-19 16:50:16 +0200

    Back to development version.

--- plumbing/__init__.py
+++ plumbing/__init__.py
@@ -1,2 +1,2 @@
 # Special variables #
-__version__ = '2.9.10'
+__version__ = '2.9.11'
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name             = 'plumbing',
-    version          = '2.9.10',
+    version          = '2.9.11',
     description      = 'Helps with plumbing-type programing in python.',
     license          = 'MIT',
     url              = 'http://github.com/xapple/plumbing/',
